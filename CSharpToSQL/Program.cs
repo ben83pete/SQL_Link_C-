e3b8e7b99649c803a1685b886144d994eb27f951
ben Petersen <ben83pete@gmail.com>
2019-02-05 12:19:17 -0500

    first push

--- CSharpToSQL/Program.cs
+++ CSharpToSQL/Program.cs
@@ -3,13 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace CSharpToSQL
 {
     class Program
-    {
+    {       // SQL- STUDENT-FLEX\SQLEXPRESS
         static void Main(string[] args)
         {
+            var connStr = @"server = STUDENT-FLEX\SQLEXPRESS; database = prsdb; trusted_connection = true";
+            // instead of trusted connection, "uid = userName; pwd = passeord;
+
+            var Connection = new SqlConnection(connStr);
+            Connection.Open();
+            if (Connection.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open.");
+                return;
+            }
+
+            var sql = "select * from users;";
+
+            var cmd = new SqlCommand(sql, Connection);
+
+            var reader = cmd.ExecuteReader();
+
+            if (!reader.HasRows)  // (reader.HasRows == false)
+            {
+                Console.WriteLine("Result set has no rows.");
+                Connection.Close();
+                return;
+            }
+
+            var users = new User[10];  // create an array to hold the user data were pulling from SQL
+            var index = 0;
+            
+            while (reader.Read())
+            {
+                var user = new User();
+                user.Id = (int)reader["Id"];
+                user.Username = (string)reader["Username"];
+                user.Firstname = (string)reader["First_Name"];
+                user.Lastname = (string)reader["Last_Name"];
+                //var Fullname = $"{Firstname} {Lastname}";
+                user.Phone = reader["Phone"] == DBNull.Value ? null : (string)reader["Phone"];
+                user.Email = reader["Email"] == DBNull.Value ? null : (string)reader["Email"];
+                // read email col in sql == check for Null ? if true set null in C# : if false (not null) set data to variable          
+                users[index++] = user;                
+
+                Console.WriteLine($"Id = {user.Id}, Name = {user.Firstname} {user.Lastname}, User Name = {user.Username}, Phone = {user.Phone}");
+                
+            }
+            Console.ReadKey();
+
+
+
+            Connection.Close();
         }
     }
 }
---
+++ CSharpToSQL/user.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToSQL
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+
+        public User( int id, string username, string firstname, string lastname, string phone, string email)
+        {
+            Id = id;
+            Username = username;
+            Firstname = firstname;
+            Lastname = lastname;
+            Phone = phone;
+            Email = email;
+        }
+
+    }
+}
